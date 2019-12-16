1f7d7af59a00cde64983c2e0ee2810e2d27013eb
LumiereXyloto <740207968@qq.com>
2019-12-16 22:48:21 +0800

    修改

--- 1.js
+++ 1.js
@@ -6,10 +6,20 @@ Array.prototype.myMap = function (fn, ctx) {
     realThis.reduce((prev, curr, index) => {
       console.log(curr)
       res.push(fn(curr))
-    })
+    }, null)
     return res
   }
   throw new Error('arg2 must be an array')
 }
 
 console.log([1,2].myMap((a) => a*2))
+
+function print(n){
+  setTimeout((() => {
+   console.log(n)
+  })(n), Math.floor(Math.random() * 1000))
+}
+  for(var i = 0; i < 100; i++){
+  print(i);
+  }
+
